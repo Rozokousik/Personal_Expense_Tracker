0734b468019cf43f646f674f308875e2d26368de
Hari-710 <harishjayaraj0710004@gmail.com>
2025-10-11 10:39:29 +0530

    Removed some imported modules

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
-import smtplib
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
 import matplotlib.pyplot as plt
 from decimal import Decimal
 import time
